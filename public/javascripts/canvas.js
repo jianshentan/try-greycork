7f663eac9da18bf2741de4a87ac5ad67f13fe4d4
jianshentan <jianshentan@gmail.com>
2016-10-23 08:06:04 +0000

    checkpoint - save image works

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -1,14 +1,20 @@
+/* globals $ */
+
 $(document).ready(function() {
-  
-  // GLOBALS 
-  var editor = $("#editor-middle");
-  var sizeMultiplier = 4;
-  
+   
+  var CAMERA_BUTTON_ID = 'camera';
+  var SAVE_BUTTON_ID = 'save';
+  var EDITOR_CONTAINER_ID = 'editor-middle';
+  var CANVAS_ID = 'canvas';
+  var SAVED_IMAGE_ID = 'saved-img';
+ 
+  var editor = $("#"+EDITOR_CONTAINER_ID);
+  var sizeMultiplier = 2;
+ 
   /**
    * Take picture / Upload picture and put picture in the background canvas
    **/
-  var camera = document.getElementById('camera');
-  var frame = document.getElementById('frame');
+  var camera = document.getElementById(CAMERA_BUTTON_ID);
   
   camera.addEventListener('change', function(e) {
     var file = e.target.files[0]; 
@@ -24,18 +30,27 @@ $(document).ready(function() {
       if (data.exif) {
         options.orientation = data.exif.get('Orientation');
       }
-      loadImage(file, function(canvas) {
-        // delete canvas if canvas already exists
-        editor.find("#canvas").remove();
-        // create canvas
-        $(canvas).attr('id', 'canvas')
-        $(canvas).attr('draggable', 'false')
-        editor.append(canvas);
-        $(window).trigger("canvasReady", [ /* param1, param2 */])
+      loadImage(file, function(tempCanvas) {
         
-        // resize canvas display to fit screen
+        /**
+         * We need to draw the resulting canvas (tempCanvas) into
+         * our canvas object, instead of just adding the tempCanvas
+         * to the page because the tempCanvas has exif meta data
+         * attached to it, complicating rotations...
+         * By drawing the tempCanvas into our canvas element, we
+         * flatten the image thereby mitigating rotation complexities
+         **/
+         
+        var canvas = document.getElementById(CANVAS_ID);
+        canvas.width = tempCanvas.width;
+        canvas.height = tempCanvas.height;
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(tempCanvas, 0, 0);
         $(canvas).css("width", ($(canvas).width() / sizeMultiplier) + "px")
+        //$(canvas).css("height", ($(canvas).height() / sizeMultiplier) + "px")
         
+        // canvas is ready!
+        $(window).trigger("canvasReady", [ /* param1, param2 */])
       }, options);
     });
   });
@@ -43,14 +58,14 @@ $(document).ready(function() {
   $(window).on("canvasReady", function(e, p1, p2) {
 
     var stickerStack = [];
-    var sticker1 = new Sticker("../images/square.png", "#editor-middle");
-    //var sticker2 = new Sticker("../images/camera.png", "#editor-middle");
+    var sticker1 = new Sticker("../images/square.png", "#"+EDITOR_CONTAINER_ID);
+    //var sticker2 = new Sticker("../images/camera.png", "#"+EDITOR_CONTAINER_ID);
     
     stickerStack.push(sticker1);
     //stickerStack.push(sticker2);
     
-    $("#save").click(function() {
-      var destinationCanvas = document.getElementById('canvas');
+    $("#"+SAVE_BUTTON_ID).click(function() {
+      var destinationCanvas = document.getElementById(CANVAS_ID);
       var destinationCtx = destinationCanvas.getContext('2d');
       destinationCtx.globalCompositeOperation = 'source-atop';
       
@@ -77,19 +92,22 @@ $(document).ready(function() {
         destinationCtx.drawImage(imgCanvas, 0, 0);
         
         destinationCtx.restore();
+        
+        // hide sticker after flattening
+        $(stickerStack[i].getSticker()).hide()
       }
       
-      /*
       // convert to image
       var dataURL = destinationCanvas.toDataURL();  
       $(destinationCanvas).css("display", "none");
-      var img = document.createElement('img');
-      img.id = "savedImg";
+      var img = document.getElementById(SAVED_IMAGE_ID);
       img.src = dataURL;
+      $(img).css("width", $(destinationCanvas).width());
+      $(img).css("height", $(destinationCanvas).height());
+      
       editor.append(img);
       
-      $("#save").off();
-      */
+      $("#"+SAVE_BUTTON_ID).off();
     });
    
   });
